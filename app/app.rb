3e7ce241770a0cdb65c114557b580986d87fc467
ViolaCrellin <viola.crellin@gmail.com>
2016-02-11 18:39:40 +0000

    Added ability to keep user's email when re-entering password

--- app/app.rb
+++ app/app.rb
@@ -21,9 +21,9 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/sign_up' do
-    user = User.new(user_name: params[:user_name], password: params[:password], email: params[:email], password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_name: params[:user_name], password: params[:password], email: params[:email], password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
       redirect '/welcome'
     else
       flash.now[:notice]= 'Ooops, your passwords didn\'t match'
